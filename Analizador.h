8a0420db2b117c93c1590c0459e40d9e5fd6699c
dianadsp <diana.patino@ucsp.edu.pe>
2019-10-03 18:14:11 -0500

    refresh

---
+++ Analizador.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iterator> 
+#include <map> 
+#include "gramatica.h"
+#include "produccion.h"
+
+using namespace std;
+class AnalizadorSintacticoLL1{
+private:
+  Gramatica gr;
+  map<string, map<string, vector<string>>> tas;
+  void adicionar(string noTerminal, string terminal, string* prod);
+  const string DOLAR = "$";
+public:
+  void llenarTas();
+  bool reconocerSentencia(string sentencia);
+};
